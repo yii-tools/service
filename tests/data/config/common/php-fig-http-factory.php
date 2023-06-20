847ace1666d83b1cf021758edab275380a9f2661
Wilmer Arambula <terabytesoftw@gmail.com>
2023-06-20 09:23:20 -0400

    Better naming config.

--- tests/data/config/common/php-fig-http-factory.php
+++ tests/data/config/common/php-fig-http-factory.php
@@ -2,9 +2,12 @@
 
 declare(strict_types=1);
 
+use HttpSoft\Message\ResponseFactory;
 use HttpSoft\Message\StreamFactory;
+use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\StreamFactoryInterface;
 
 return [
+    ResponseFactoryInterface::class => ResponseFactory::class,
     StreamFactoryInterface::class => StreamFactory::class,
 ];
